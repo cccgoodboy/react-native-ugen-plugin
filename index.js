24cd1f5e3fa31c1db80de5f233807791737044e6
初程程 <hatsuhodohodo@chuchengchengdeMacBook-Pro.local>
2019-04-01 20:49:17 +0800

    11

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
 
-import { NativeModules,NativeEventEmitter } from 'react-native';
+import { NativeModules,NativeEventEmitter,Platform,DeviceEventEmitter } from 'react-native';
 const sdk = NativeModules.AliLiving;
-
+// const listen = NativeModules.NativeListener;
+// const listenModule = new NativeEventEmitter(listen);
+const LISTEN = [
+    "onPreCheck",                 	//参数检测回调 
+    "onProvisionPrepare",         	//提示用户输入账号密码 广播配网方式的时候会有这个回调
+    "onProvisioning",             	//配网中
+    "onProvisionStatus", 		 	//配网状态
+    "onProvisionedResult", 			//配网结果
+    "error"];
 class AliLiving{
     //登录
     static async login(){
@@ -17,15 +25,45 @@ class AliLiving{
         return sdk.isLogin();
     }
     //开始搜寻设备
-    static startScanLocalDevice(){
+    static startScanLocalDevice(callback){
+        // if(Platform.OS === "android"){
+        //     DeviceEventEmitter.addListener('scanLocationDevice',(device)=>{
+        //         callback(device);
+        //     });
+        // }else{
+        //     listenModule.addListener("scanLocationDevice",(device)=>{
+        //         callback(device);
+        //     });
+        // }
         sdk.startScanLocalDevice();
     }
     //停止搜寻设备
     static stopScanLocalDevice(){
+        // if(Platform.OS === 'android'){
+        //     DeviceEventEmitter.removeAllListeners('scanLocationDevice');
+        // }else{
+        //     listenModule.removeAllListeners('scanLocationDevice');
+        // }
         sdk.stopScanLocalDevice();
     }
     //添加配网设备
-    static startAddDevice(productKey){
+    static startAddDevice(productKey,success,error){
+        // if(Platform.OS === "android"){
+        //     DeviceEventEmitter.addListener('onProvisionedResult',(res)=>{
+        //         success(res);
+        //     });
+        //     DeviceEventEmitter.addListener('error',(res)=>{
+        //         error(res);
+        //     });
+        // }else{
+        //     listenModule.addListener("onProvisionedResult",(res)=>{
+        //         success(res);
+        //     });
+        //     listenModule.addListener("error",(res)=>{
+        //         error(res);
+        //     });
+        // }
+        console.warn('pk',productKey);
         sdk.startAddDevice(productKey);
     }
     //中止配网流程
@@ -62,3 +100,4 @@ class AliLiving{
     }
 }
 export default AliLiving;
+
