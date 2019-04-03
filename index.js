ad4d53007ed36a9ea0f3bd5030bb8562004ff04f
初程程 <hatsuhodohodo@chuchengchengdeMacBook-Pro.local>
2019-04-03 10:28:08 +0800

    chang index

--- index.js
+++ index.js
@@ -2,16 +2,13 @@
 
 import { NativeModules,NativeEventEmitter,Platform,DeviceEventEmitter } from 'react-native';
 const sdk = NativeModules.UgenAliLiving;
-// const listen = NativeModules.NativeListener;
-// const listenModule = new NativeEventEmitter(listen);
-const LISTEN = [
-    "onPreCheck",                 	//参数检测回调 
-    "onProvisionPrepare",         	//提示用户输入账号密码 广播配网方式的时候会有这个回调
-    "onProvisioning",             	//配网中
-    "onProvisionStatus", 		 	//配网状态
-    "onProvisionedResult", 			//配网结果
-    "error"];
+const listen = NativeModules.NativeListener;
+const listenModule = new NativeEventEmitter(listen);
 class AliLiving{
+    //初始化SDK
+    static instanceSDK(){
+        sdk.instanceSDK();
+    }
     //登录
     static async login(){
         return sdk.login();
@@ -26,44 +23,43 @@ class AliLiving{
     }
     //开始搜寻设备
     static startScanLocalDevice(callback){
-        // if(Platform.OS === "android"){
-        //     DeviceEventEmitter.addListener('scanLocationDevice',(device)=>{
-        //         callback(device);
-        //     });
-        // }else{
-        //     listenModule.addListener("scanLocationDevice",(device)=>{
-        //         callback(device);
-        //     });
-        // }
+        if(Platform.OS === "android"){
+            DeviceEventEmitter.addListener('scanLocationDevice',(device)=>{
+                callback(device);
+            });
+        }else{
+            listenModule.addListener("scanLocationDevice",(device)=>{
+                callback(device);
+            });
+        }
         sdk.startScanLocalDevice();
     }
     //停止搜寻设备
     static stopScanLocalDevice(){
-        // if(Platform.OS === 'android'){
-        //     DeviceEventEmitter.removeAllListeners('scanLocationDevice');
-        // }else{
-        //     listenModule.removeAllListeners('scanLocationDevice');
-        // }
+        if(Platform.OS === 'android'){
+            DeviceEventEmitter.removeAllListeners('scanLocationDevice');
+        }else{
+            listenModule.removeAllListeners('scanLocationDevice');
+        }
         sdk.stopScanLocalDevice();
     }
     //添加配网设备
     static startAddDevice(productKey,success,error){
-        // if(Platform.OS === "android"){
-        //     DeviceEventEmitter.addListener('onProvisionedResult',(res)=>{
-        //         success(res);
-        //     });
-        //     DeviceEventEmitter.addListener('error',(res)=>{
-        //         error(res);
-        //     });
-        // }else{
-        //     listenModule.addListener("onProvisionedResult",(res)=>{
-        //         success(res);
-        //     });
-        //     listenModule.addListener("error",(res)=>{
-        //         error(res);
-        //     });
-        // }
-        console.warn('pk',productKey);
+        if(Platform.OS === "android"){
+            DeviceEventEmitter.addListener('onProvisionedResult',(res)=>{
+                success(res);
+            });
+            DeviceEventEmitter.addListener('error',(res)=>{
+                error(res);
+            });
+        }else{
+            listenModule.addListener("onProvisionedResult",(res)=>{
+                success(res);
+            });
+            listenModule.addListener("error",(res)=>{
+                error(res);
+            });
+        }
         sdk.startAddDevice(productKey);
     }
     //中止配网流程
