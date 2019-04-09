dd15612b242655d704e48469ccf7f7c0c023fc64
初程程 <hatsuhodohodo@chuchengchengdeMacBook-Pro.local>
2019-04-09 17:38:38 +0800

    save

---
+++ ios/AliLiving/AliLiving.h
@@ -0,0 +1,13 @@
+//
+//  AliLiving.h
+//  AliLiving
+//
+//  Created by 初程程 on 2019/4/9.
+//  Copyright © 2019年 初程程. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AliLiving : NSObject
+
+@end
